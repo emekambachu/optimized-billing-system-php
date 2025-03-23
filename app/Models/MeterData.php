928a33ce73da7c665c545632e78213334646d46a
Victor Emeka Mbachu <victor.c.mbachu@gmail.com>
2025-03-23 14:25:27 +0000

    updateing meter data to reduce complexity

--- app/Models/MeterData.php
+++ app/Models/MeterData.php
@@ -1,33 +1,16 @@
 <?php
-declare(strict_types=1);
-
 namespace App\Models;
 
 class MeterData
 {
-    private int $meterId;
-    private \DateTime $timestamp;
-    private float $reading;
+    public int $meterId;
+    public string $timestamp;
+    public float $meterReading;
 
-    public function __construct(int $meterId, \DateTime $timestamp, float $reading)
+    public function __construct(int $meterId, string $timestamp, float $meterReading)
     {
         $this->meterId = $meterId;
         $this->timestamp = $timestamp;
-        $this->reading = $reading;
-    }
-
-    public function getMeterId(): int
-    {
-        return $this->meterId;
-    }
-
-    public function getTimestamp(): \DateTime
-    {
-        return $this->timestamp;
-    }
-
-    public function getReading(): float
-    {
-        return $this->reading;
+        $this->meterReading = $meterReading;
     }
 }
--- app/Services/RateStrategyInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-namespace App\Services;
-
-interface RateStrategyInterface
-{
-    public function getRate(\DateTime $timestamp): float;
-}
